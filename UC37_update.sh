a686ebc80db51ecf17c94df0b51a5efefc8a2581
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2022-08-20 15:18:11 -0400

    Update UC37_update.sh

--- UC37_update.sh
+++ UC37_update.sh
@@ -6,6 +6,6 @@ git clone https://github.com/Mrpi314tech/UC37software
 mv ~/new_words.py ~/UC37software/Python
 mv ~/info.py ~/UC37software/Welcome
 rm ~/UC37software/UC37_update.sh
-chmod + ~/UC37software/UC37
+chmod +x ~/UC37software/UC37
 chmod +x ~/UC37software/UC37_remove.sh
 chmod +x ~/UC37software/UC37_install.sh
