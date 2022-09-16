9f02d75172e73f3e703f09acd189d317c6278eb0
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2022-09-16 18:16:49 -0400

    Add files via upload

--- UC37_update.sh
+++ UC37_update.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 mv ~/UC37software/Python/new_words.py ~
 mv ~/UC37software/Welcome/info.py ~
+mv ~/UC37software/Python/new_com.py ~
 rm -r ~/UC37software
 git clone https://github.com/Mrpi314tech/UC37software
 rm ~/UC37software/Python/new_words.py
 rm ~/UC37software/Welcome/info.py
 mv ~/new_words.py ~/UC37software/Python
 mv ~/info.py ~/UC37software/Welcome
+mv ~/new_com.py ~/UC37software/Python
 rm ~/UC37software/UC37_update.sh
 chmod +x ~/UC37software/UC37
 chmod +x ~/UC37software/UC37_remove.sh
