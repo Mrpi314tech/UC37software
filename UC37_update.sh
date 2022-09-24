63004e744349ef7dbb6cd89260312c8c195f2bd5
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2022-09-24 18:32:41 -0400

    Add files via upload

--- UC37_update.sh
+++ UC37_update.sh
@@ -10,10 +10,12 @@ rm ~/UC37software/Welcome/info.py
 mv ~/new_words.py ~/UC37software/Python
 mv ~/info.py ~/UC37software/Welcome
 mv ~/new_com.py ~/UC37software/Python
-mv ~/UC37_update.sh ~/delete_this_file(UC37)
-mv ~/UC37software/UC37_update.sh ~
+mv ~/UC37_update.sh ~/delete_this_file-UC37
 chmod +x ~/UC37software/UC37
 chmod +x ~/UC37software/UC37_remove.sh
 chmod +x ~/UC37software/UC37_install.sh
 echo 'type "y" for the next 2 questions: '
 rm -r ~/UC37software/.git
+chmod +x ~/UC37software/UC37_update.sh
+mv ~/UC37software/UC37_update.sh ~
+mv ~/UC37_update.sh ~/delete_this_file-UC37
