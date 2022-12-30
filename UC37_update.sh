bf5a3993d93cb68461146d2ac420b248075b86d8
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2022-12-30 18:21:58 -0500

    Update UC37_update.sh

--- UC37_update.sh
+++ UC37_update.sh
@@ -2,11 +2,16 @@
 mv ~/UC37software/Python/new_words.py ~
 mv ~/UC37software/Welcome/info.py ~
 mv ~/UC37software/Python/new_com.py ~
+mv ~/UC37software/Python/skills ~
 echo 'type "y" if it asks you a question: '
 rm -r ~/UC37software
 git clone https://github.com/Mrpi314tech/UC37software
 rm ~/UC37software/Python/new_words.py
 rm ~/UC37software/Welcome/info.py
+rm ~/skills/install_skills.py
+mv ~/UC37software/Python/skills/install_skills.py ~/skills
+rm -r ~/UC37software/Python/skills
+mv ~/skills ~/UC37software/Python
 mv ~/new_words.py ~/UC37software/Python
 mv ~/info.py ~/UC37software/Welcome
 mv ~/new_com.py ~/UC37software/Python
