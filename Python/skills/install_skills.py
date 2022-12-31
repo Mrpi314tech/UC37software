8c6101c3993d1a9749842f211a7c3ad7b50c9277
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2022-12-30 19:28:32 -0500

    Update install_skills.py

--- Python/skills/install_skills.py
+++ Python/skills/install_skills.py
@@ -6,7 +6,7 @@ nwcoml=acom.word
 nrunl=acom.com
 file_location=os.path.expanduser('~')
 qstn=input('what skill are you adding?')
-if 'kasa' in qstn:
+if 'kasa' in qstn or 'Kasa' in qstn:
     print('You will need to install the python-kasa library with pip')
     os.system('wget https://raw.githubusercontent.com/Mrpi314tech/UC37skills/main/kasa_skills.py ~/UC37software/Python/skills')
     alias=input('what is the name of the device? ')
@@ -19,7 +19,7 @@ if 'kasa' in qstn:
     file1 = open(file_location+"/UC37software/Python/new_com.py", "w")
     file1.write("word="+str(nwcoml)+"\ncom="+str(nrunl))
     file1.close()
-elif 'timer' in qstn:
+elif 'timer' in qstn or 'Timer' in qstn:
     os.system('wget https://raw.githubusercontent.com/Mrpi314tech/UC37skills/main/timer.py ~/UC37software/Python/skills')
     ringer=input('what mp3 file do you want to play when the timer is over? ')
     file = open(file_location+"/UC37software/Python/skills/skill_data.py", "a")
@@ -30,3 +30,5 @@ elif 'timer' in qstn:
     file1 = open(file_location+"/UC37software/Python/new_com.py", "w")
     file1.write("word="+str(nwcoml)+"\ncom="+str(nrunl))
     file1.close()
+else:
+    print('Skill not found! Maybe time to update?')
