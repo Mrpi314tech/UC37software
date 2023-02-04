ff3aef7cc94bcaa20e006b9c28516f64b30090cc
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2023-02-04 10:41:39 -0500

    UC37 is now under "System Tools" category

--- UC37_update.sh
+++ UC37_update.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+location="$HOME"
 mv ~/UC37software/Python/new_words.py ~
 mv ~/UC37software/Welcome/info.py ~
 mv ~/UC37software/Python/new_com.py ~
 mv ~/UC37software/Python/skills ~
 echo 'type "y" if it asks you a question: '
+rm ~/.local/share/applications/UC37.desktop
 rm -r ~/UC37software
 git clone https://github.com/Mrpi314tech/UC37software
 rm ~/UC37software/Python/new_words.py
@@ -16,6 +18,19 @@ mv ~/new_words.py ~/UC37software/Python
 mv ~/info.py ~/UC37software/Welcome
 mv ~/new_com.py ~/UC37software/Python
 mv ~/UC37_update.sh ~/delete_this_file-UC37
+mkdir -p ~/.local/share/applications
+echo "[Desktop Entry]
+Name=UC37
+Comment=UC37 the AI
+Exec=UC37
+Icon=${location}/UC37software/images/UC37.png
+Terminal=false
+Type=Application
+Categories=System;
+StartupNotify=true" > ~/.local/share/applications/UC37.desktop
+echo "done"
+echo "UC37 is installed!"
+echo "The UC37 app can be found in Menu>System Tools>UC37"
 chmod +x ~/UC37software/UC37
 chmod +x ~/UC37software/UC37_remove.sh
 chmod +x ~/UC37software/UC37_install.sh
