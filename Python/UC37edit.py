0b6578bd930b18e1872dfdc7ad7e686980ef7db9
Mrpi314tech <elliotedebruin@gmail.com>
2023-02-06 20:00:05 -0500

    Fix bugs

--- Python/UC37edit.py
+++ Python/UC37edit.py
@@ -10,7 +10,7 @@ nrunl=acom.com
 file_location=os.path.expanduser('~')
 qstn=input(" are you adding a command, music, or just words? ")
 if 'com' in qstn or 'Com' in qstn:
-    kwordc=input("what is the keyword? (what do you want to say to run the command?)")
+    kwordc=input("what is the keyword? (what do you want to say to run the command?)").lower()
     outputc=input("what command do you want to run?")
     outc='os.system("'+outputc+'")'
     nwcoml.append(kwordc)
@@ -19,7 +19,7 @@ if 'com' in qstn or 'Com' in qstn:
     file1.write("word="+str(nwcoml)+"\ncom="+str(nrunl))
     file1.close()
 elif 'wor' in qstn or 'Wor' in qstn:
-    kwordw=input("what is the keyword? (what do you want to say so that he says this?)")
+    kwordw=input("what is the keyword? (what do you want to say so that he says this?)").lower()
     outputw=input("what do you want him to say?")
     nwordl.append(kwordw)
     ndefl.append(outputw)
@@ -27,9 +27,9 @@ elif 'wor' in qstn or 'Wor' in qstn:
     file1.write("word="+str(nwordl)+"\ndefi="+str(ndefl))
     file1.close()
 elif 'mus' in qstn or 'Mus' in qstn:
-    kwordc=input("what is the keyword? (what do you want to say to play the music?)")
+    kwordc=input("what is the keyword? (what do you want to say to play the music?)").lower()
     outputc=input("what is the file location for the music and the name?")
-    outc='vlc '+outputc
+    outc='vlc '+outputc+' &'
     nwcoml.append(kwordc)
     nrunl.append(outc)
     file1 = open(file_location+"/UC37software/Python/new_com.py", "w")
