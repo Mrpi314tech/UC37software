4ed1704dc140a13c106adc6c11e626d5d33f6fbd
Mrpi314tech <elliotedebruin@gmail.com>
2023-03-02 15:48:51 -0500

    add remove function to edit

--- Python/UC37edit.py
+++ Python/UC37edit.py
@@ -8,12 +8,12 @@ ndefl=aword.defi
 nwcoml=acom.word
 nrunl=acom.com
 file_location=os.path.expanduser('~')
-qstn=input(" are you adding a command, music, or just words? ")
+qstn=input(" are you adding a command, music, words, or would you like to remove words? ")
 if 'com' in qstn or 'Com' in qstn:
     kwordc=input("what is the keyword? (what do you want to say to run the command?)").lower()
     outputc=input("what command do you want to run?")
     outc='os.system("'+outputc+'")'
-    nwcoml.append(kwordc)
+    nwcoml.append(kwordc.lower())
     nrunl.append(outputc)
     file1 = open(file_location+"/UC37software/Python/new_com.py", "w")
     file1.write("word="+str(nwcoml)+"\ncom="+str(nrunl))
@@ -21,7 +21,7 @@ if 'com' in qstn or 'Com' in qstn:
 elif 'wor' in qstn or 'Wor' in qstn:
     kwordw=input("what is the keyword? (what do you want to say so that he says this?)").lower()
     outputw=input("what do you want him to say?")
-    nwordl.append(kwordw)
+    nwordl.append(kwordw.lower())
     ndefl.append(outputw)
     file1 = open(file_location+"/UC37software/Python/new_words.py", "w")
     file1.write("word="+str(nwordl)+"\ndefi="+str(ndefl))
@@ -35,3 +35,50 @@ elif 'mus' in qstn or 'Mus' in qstn:
     file1 = open(file_location+"/UC37software/Python/new_com.py", "w")
     file1.write("word="+str(nwcoml)+"\ncom="+str(nrunl))
     file1.close()
+elif 'rem' in qstn:
+    print('here is a list of the words: ')
+    seelist=0
+    while True:
+        try:
+            print(nwordl[seelist]+" -- "+ndefl[seelist])
+            seelist+=1
+        except IndexError:
+            break
+    seelist=0
+    while True:
+        try:
+            print(nwcoml[seelist]+" -- "+nrunl[seelist])
+            seelist+=1
+        except IndexError:
+            break
+    removew=input("what word/command are you removing? ")
+    seelist=0
+    while True:
+        try:
+            if nwordl[seelist]==removew:
+                print(nwordl[seelist])
+                nwordl.remove(nwordl[seelist])
+                ndefl.remove(ndefl[seelist])
+                file1 = open(file_location+"/UC37software/Python/new_words.py", "w")
+                file1.write("word="+str(nwordl)+"\ndefi="+str(ndefl))
+                file1.close()
+                break
+            else:
+                seelist+=1
+        except IndexError:
+            seelist=0
+            while True:
+                try:
+                    if nwcoml[seelist]==removew:
+                        nwordl.remove(nwcoml[seelist])
+                        nrunl.remove(nrunl[seelist])
+                        file1 = open(file_location+"/UC37software/Python/new_com.py", "w")
+                        file1.write("word="+str(nwcoml)+"\ncom="+str(nrunl))
+                        file1.close()
+                        break
+                    else:
+                        seelist+=1
+                except IndexError:
+                    print('word/command not found!')
+                    break
+            break
