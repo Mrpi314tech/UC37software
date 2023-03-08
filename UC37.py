79fa76dea908a1ae7c8f3b40bf6ec4343ef146e9
Mrpi314sch <115723525+Mrpi314sch@users.noreply.github.com>
2023-03-08 11:07:14 -0500

    Delete UC37.py

--- UC37.py
+++
@@ -1,9 +0,0 @@
-#Mrpi314 programming
-#Modes: Windows, Crowpi2, or Simple.
-mode='Windows'
-if mode == 'Windows':
-    import Python.UC37_Windows as UC37
-elif mode == 'Crowpi2':
-    import Python.UC37_full as UC37
-elif mode == 'Simple':
-    import Python.UC37_mini.py as UC37
