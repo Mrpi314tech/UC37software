04ae0f96c7bf56c40bd0ee5d8c2b5b5f50e9fe14
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2023-04-07 12:11:15 -0400

    Update Survey.py

--- Welcome/Survey.py
+++ Welcome/Survey.py
@@ -3,8 +3,8 @@ print(os.path.expanduser('~'))
 file_location=os.path.expanduser('~')
 your_name = input('what is your name? ')
 name='UC37'# what do you want the name of the robot to be?
-city=input('what city are you closest too? (for finding the weather) ')
-api=input("what is your weather api key? ") 
+city="blah"
+api="blah" 
 password=input('what do you want the password to be?' )
 ask_for_password=input('do you want to be asked for the password? (True/False) ')
 ip_for_kasa=" "
